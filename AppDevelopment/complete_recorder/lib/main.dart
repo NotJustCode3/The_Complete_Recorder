fe23079249e885af0fd9130cc72666a19a0cadd3
Shafiq Zainuddin <zainshafiq18@yahoo.com>
2021-01-25 17:46:37 -0600

    Latest Page

--- AppDevelopment/complete_recorder/lib/main.dart
+++ AppDevelopment/complete_recorder/lib/main.dart
@@ -3,10 +3,146 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+      MaterialApp(
+        home: Record()
+      )
+  );
 }
 
-class MyApp extends StatelessWidget {
+class Record extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //Accessing the home of the material widget and drawing the scaffold onto it
+      home: Scaffold(
+        backgroundColor:
+        Colors.black, //the background color of the scaffold is white
+        body: SafeArea(
+        //creating a sfe area to start drawing elements on to the scaffold widget
+        child: Column(children: [
+          Card(
+          //creating a child relationship with the safe area to include our card widget
+          //card widget properties
+          elevation: 3.0, //this gives it a slight drop shadow effect
+          color: Colors.grey[600],
+          margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+          //creating another child to draw elements onto the card widget
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border(
+                top: BorderSide(width: 2.0, color: Colors.white),
+                bottom: BorderSide(width: 2.0, color: Colors.white),
+                right: BorderSide(width: 2.0, color: Colors.white),
+                left: BorderSide(width: 2.0, color: Colors.white),
+                ),
+            ),
+            child: ListTile(
+              //this widget houses the elements inside the card widget
+              leading: Icon(Icons.menu,
+              color: Colors.white
+              ), //the leading property takes care of the elements to the left of the card
+              //a simple text widget to write and style our text
+              title: Text(
+               'Search',
+                style: TextStyle(
+                color: Colors.white,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Roboto',
+                letterSpacing: 0.5),
+              ),
+              trailing: Icon(Icons.create_new_folder_outlined,
+              size: 30.0,
+              color: Colors
+              .white
+              ), //the trailing property takes care of elements to the right of the card
+            ),
+          ),
+        ),
+
+        //ROW 4: RECORDING BUTTON THAT RECORDS
+        Container(
+          margin: EdgeInsets.only(top: 5.0, bottom: 5.0),
+          child: Container(
+            margin: EdgeInsets.only(top: 100.0, bottom: 20.0),
+            child: Center(
+              child: Ink(
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.circular(100),
+                  border: Border.all(
+                  width: 5,
+                  color: Colors.white,
+                  )
+                  //shape: CircleBorder(),
+                ),
+                child: IconButton(
+                  icon: Icon(Icons.mic),
+                  iconSize: 70.0,
+                  color: Colors.white,
+                  highlightColor: Colors.grey,
+                  //splashColor: Colors.grey,
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Library())
+                    );
+                  },
+                ),
+              ),
+            ),
+          ),
+        ),
+
+        const Divider(
+          color: Colors.redAccent,
+          height: 20,
+          thickness: 3,
+          indent: 60,
+          endIndent: 60,
+        ),
+
+          Container(
+            margin: EdgeInsets.only(top: 5.0, bottom: 5.0),
+            child: Container(
+              margin: EdgeInsets.only(top: 20.0, bottom: 20.0),
+              child: Center(
+                child: Ink(
+                  decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(100),
+                      border: Border.all(
+                        width: 5,
+                        color: Colors.white70,
+                      )
+                    //shape: CircleBorder(),
+                  ),
+                  child: IconButton(
+                    icon: Icon(Icons.library_books),
+                    iconSize: 70.0,
+                    color: Colors.white,
+                    highlightColor: Colors.grey,
+                    //splashColor: Colors.grey,
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => Library())
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ])),
+      ),
+    );
+  }
+}
+
+
+class Library extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -249,35 +385,14 @@ class MyApp extends StatelessWidget {
                   color: Colors.white,
                   highlightColor: Colors.grey,
                   //splashColor: Colors.grey,
-                  onPressed: () {},
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
                 ),
               ),
             ),
           ),
         ])),
-      ),
-    );
-  }
-}
-
-
-class FirstRoute extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('First Route'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          child: Text('Open route'),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => MyApp()),
-            );
-          },
-        ),
       ),
     );
   }
