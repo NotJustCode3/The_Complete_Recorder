a34015f00b16aede05fa421362b5dea2c7dd23dd
azeezatl <azeezat.a.lawal@gmail.com>
2021-03-01 01:37:26 -0700

    Revert "Merge branch 'develop' into feature-azeezat-record"
    This reverts commit f658d5f7e5b6e53ed7328a46141088ac49a7e3cd, reversing
    changes made to 5a40f98e35c88d9a915ad911d0ef3ce3a63dd8f7.

--- AppDevelopment/complete_recorder/lib/main.dart
+++ AppDevelopment/complete_recorder/lib/main.dart
@@ -7,7 +7,6 @@ void main() {
   runApp(MaterialApp(home: Homepage()));
 }
 
-
 //PAGE 2 - LIBRARY, FOLDERS, ETC...
 class Homepage extends StatelessWidget {
   // This widget is the root of your application.
@@ -37,7 +36,13 @@ class Homepage extends StatelessWidget {
                       color: Colors.grey,
                       //highlightColor: Colors.grey,
                       onPressed: () {
-
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => Homepage()));
+                      }),
+                  title: Text(
+                    'Search',
                     style: TextStyle(
                         color: Colors.grey,
                         fontFamily: 'Roboto',
@@ -267,10 +272,10 @@ class Homepage extends StatelessWidget {
                     ),
                   ),
                 ),
-
+              ),
+            ],
           ),
         ),
-
       ),
     );
   }
