00ae04007058e50d9bdffbe924165a240cc3411d
Shafiq Zainuddin <zainshafiq18@yahoo.com>
2021-04-09 20:20:53 -0600

    Committing Wave UI

--- AppDevelopment/complete_recorder/lib/App Pages/recording_page.dart
+++ AppDevelopment/complete_recorder/lib/App Pages/recording_page.dart
@@ -1,5 +1,6 @@
 // TODO Implement this library.
 import 'dart:io';
+import 'package:audio_wave/audio_wave.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_audio_recorder/flutter_audio_recorder.dart';
 import 'package:path_provider/path_provider.dart';
@@ -29,7 +30,9 @@ enum RecordingState {
   Stopped,
 }
 
+
 class _RecordPageState extends State<RecordPage> {
+  int _waveDuration = 170;
   IconData _recordIcon = Icons.mic_none;
   String _recordText = 'Click to start';
   RecordingState _recordingState = RecordingState.UnSet;
@@ -57,78 +60,203 @@ class _RecordPageState extends State<RecordPage> {
     super.dispose();
   }
 
+  bool _hasBeenPressed = false;
+  double _soundOpacity = 1.0;
+  int _soundDuration = 0;
+
   @override
   Widget build(BuildContext context) {
     return FractionallySizedBox(
       alignment: Alignment.center,
-      child: Container(
-        //padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
-        margin: EdgeInsets.fromLTRB(0, 340, 0, 0),
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.vertical(
-              top: Radius.circular(50),
-            ),
-            color: Color(0xFF111328),
-            // gradient: new LinearGradient(
-            //   colors: [Color.deepPurple[900]],
-            // ),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey,
-                blurRadius: 7.0,
-                spreadRadius: 2.0,
-              ),
-            ]),
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            SizedBox(
-              width: 100,
-              child: Container(
-                decoration: BoxDecoration(shape: BoxShape.circle, boxShadow: [
-                  BoxShadow(
-                    color: Colors.white,
-                    blurRadius: 5.0,
-                    spreadRadius: 4.0,
-                  ),
-                ]),
-                child: RaisedButton(
-                  color: Colors.red[700],
-                  onPressed: () async {
-                    await _onRecordButtonPressed();
-                    setState(() {});
-                  },
-                  shape: CircleBorder(
-                      //borderRadius: BorderRadius.circular(30),
-                      //side: BorderSide(color: Colors.white, width: 2.0)
+      child: Column(
+        children: [
+          Expanded(
+            child: Column(
+              children: [
+                Stack(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.fromLTRB(0, 140, 0, 0),
+                      child: AudioWave(
+                        height: 80,
+                        width: 350,
+                        alignment: 'top',
+                        spacing: 3,
+                        beatRate: Duration(milliseconds: _waveDuration),
+                        bars: [
+                          AudioWaveBar(height: 20, color: Colors.grey[800]),
+                          AudioWaveBar(height: 50, color: Colors.grey[800]),
+                          AudioWaveBar(height: 70, color: Colors.grey[800]),
+                          AudioWaveBar(height: 50, color: Colors.grey[800]),
+                          AudioWaveBar(height: 35, color: Colors.grey[800]),
+                          AudioWaveBar(height: 20, color: Colors.grey[800]),
+                          AudioWaveBar(height: 35, color: Colors.grey[800]),
+                          AudioWaveBar(height: 50, color: Colors.grey[800]),
+                          AudioWaveBar(height: 70, color: Colors.grey[800]),
+                          AudioWaveBar(height: 50, color: Colors.grey[800]),
+                          AudioWaveBar(height: 35, color: Colors.grey[800]),
+                          AudioWaveBar(height: 20, color: Colors.grey[800]),
+                          AudioWaveBar(height: 70, color: Colors.grey[800]),
+                          AudioWaveBar(height: 50, color: Colors.grey[800]),
+                          AudioWaveBar(height: 70, color: Colors.grey[800]),
+                          AudioWaveBar(height: 50, color: Colors.grey[800]),
+                          AudioWaveBar(height: 35, color: Colors.grey[800]),
+                          AudioWaveBar(height: 20, color: Colors.grey[800]),
+                          AudioWaveBar(height: 10, color: Colors.grey[800]),
+                          AudioWaveBar(height: 50, color: Colors.grey[800]),
+                          AudioWaveBar(height: 30, color: Colors.grey[800]),
+                          AudioWaveBar(height: 50, color: Colors.grey[800]),
+                          AudioWaveBar(height: 35, color: Colors.grey[800]),
+                          AudioWaveBar(height: 20, color: Colors.grey[800]),
+                          AudioWaveBar(height: 10, color: Colors.grey[800]),
+                          AudioWaveBar(height: 50, color: Colors.grey[800]),
+                          AudioWaveBar(height: 70, color: Colors.grey[800]),
+                        ],
                       ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(0, 50, 0, 0),
+                      child:
+                      AudioWave(
+                        height: 80,
+                        width: 350,
+                        alignment: 'bottom',
+                        spacing: 3,
+                        beatRate: Duration(milliseconds: _waveDuration),
+                        bars: [
+                          AudioWaveBar(height: 50, color: Colors.deepOrange),
+                          AudioWaveBar(height: 100, color: Colors.grey[200]),
+                          AudioWaveBar(height: 120, color: Colors.grey[300]),
+                          AudioWaveBar(height: 100, color: Colors.grey[400]),
+                          AudioWaveBar(height: 75, color: Colors.grey[500]),
+                          AudioWaveBar(height: 50, color: Colors.deepOrange),
+                          AudioWaveBar(height: 75, color: Colors.grey[400]),
+                          AudioWaveBar(height: 100, color: Colors.grey[300]),
+                          AudioWaveBar(height: 120, color: Colors.grey[200]),
+                          AudioWaveBar(height: 100, color: Colors.grey[300]),
+                          AudioWaveBar(height: 75, color: Colors.grey[400]),
+                          AudioWaveBar(height: 50, color: Colors.deepOrange),
+                          AudioWaveBar(height: 120, color: Colors.grey[500]),
+                          AudioWaveBar(height: 100, color: Colors.grey[400]),
+                          AudioWaveBar(height: 120, color: Colors.grey[300]),
+                          AudioWaveBar(height: 100, color: Colors.grey[200]),
+                          AudioWaveBar(height: 75, color: Colors.grey[300]),
+                          AudioWaveBar(height: 50, color: Colors.deepOrange),
+                          AudioWaveBar(height: 40, color: Colors.grey[400]),
+                          AudioWaveBar(height: 100, color: Colors.grey[500]),
+                          AudioWaveBar(height: 120, color: Colors.grey[400]),
+                          AudioWaveBar(height: 100, color: Colors.grey[300]),
+                          AudioWaveBar(height: 75, color: Colors.grey[200]),
+                          AudioWaveBar(height: 50, color: Colors.grey[300]),
+                          AudioWaveBar(height: 40, color: Colors.deepOrange),
+                          AudioWaveBar(height: 100, color: Colors.grey[200]),
+                          AudioWaveBar(height: 120, color: Colors.grey[300]),
+                        ],
+                      ),
+                    ),
+                    Container(
+                        alignment: Alignment.center,
+                        margin: EdgeInsets.fromLTRB(0, 30, 0, 0),
+                        height: 220,
+                        width: 350,
+                        child: AnimatedOpacity(
+                          opacity: _soundOpacity,
+                          duration: Duration(milliseconds: 0),
+                          child: Image(image: AssetImage('res/wave.png')),
+                        ),
+                        decoration : BoxDecoration(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(20),
+                          ),
+                          color: _hasBeenPressed ? Colors.transparent : Color(0xFF0A0E21),
+                        )
+                    ),
+                  ],
+                ),
+                Expanded(
                   child: Container(
-                    width: 150,
-                    height: 150,
-                    child: Icon(
-                      _recordIcon,
-                      size: 50,
-                      color: Colors.white,
+                    //padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                    margin: EdgeInsets.fromLTRB(0, 100, 0, 0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(50),
+                      ),
+                      color: Color(0xFF111328),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey,
+                          blurRadius: 7.0,
+                          spreadRadius: 2.0,
+                        ),
+                      ],
+                      // gradient: new LinearGradient(
+                      //   colors: [Color.deepPurple[900]],
+                      // ),
+                    ),
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        SizedBox(
+                          width: 100,
+                          child: Container(
+                            decoration: BoxDecoration(shape: BoxShape.circle, boxShadow: [
+                              BoxShadow(
+                                color: Colors.white,
+                                blurRadius: 5.0,
+                                spreadRadius: 4.0,
+                              ),
+                            ]),
+                            child: RaisedButton(
+                              color: Colors.red[700],
+                              onPressed: () async {
+                                await _onRecordButtonPressed();
+                                setState(() {
+                                  _hasBeenPressed = !_hasBeenPressed;
+                                  if(_hasBeenPressed == true){
+                                    return _soundOpacity = 0.0;
+                                  }
+                                  else if(_hasBeenPressed == false){
+                                    return _soundOpacity = 1.0;
+                                  }
+                                });
+                              },
+                              shape: CircleBorder(
+                                //borderRadius: BorderRadius.circular(30),
+                                //side: BorderSide(color: Colors.white, width: 2.0)
+                              ),
+                              child: Container(
+                                width: 150,
+                                height: 150,
+                                child: Icon(
+                                  _recordIcon,
+                                  size: 50,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Align(
+                          alignment: Alignment.bottomCenter,
+                          child: Padding(
+                              child: Text(
+                                _recordText,
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 15.0,
+                                    fontFamily: 'Roboto',
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              padding: const EdgeInsets.fromLTRB(0, 20, 0, 5)),
+                        )
+                      ],
                     ),
                   ),
                 ),
-              ),
+              ],
             ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Padding(
-                  child: Text(
-                    _recordText,
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 15.0,
-                        fontFamily: 'Roboto',
-                        fontWeight: FontWeight.bold),
-                  ),
-                  padding: const EdgeInsets.fromLTRB(0, 20, 0, 5)),
-            )
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
@@ -179,7 +307,7 @@ class _RecordPageState extends State<RecordPage> {
     Directory appDirectory = await getApplicationDocumentsDirectory();
     final currentDateTime = DateTime.now().millisecondsSinceEpoch.toString();
     DateTime recordedDate =
-        DateTime.fromMillisecondsSinceEpoch(int.parse(currentDateTime));
+    DateTime.fromMillisecondsSinceEpoch(int.parse(currentDateTime));
     final DateFormat formatter = DateFormat('yyyy-MMM-dd-hh:mm:ss');
     final String formatted = formatter.format(recordedDate);
     String filePath = appDirectory.path + '/' + currentDateTime + '-' + currentDateTime + '.aac';
